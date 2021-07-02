a377977d85cce2b0f2c4049df0fe0e133ff718e4
BarryYan <barry.yansh@gmail.com>
2021-07-02 23:19:13 +0800

    add interrupt

---
+++ os/src/interrupt/mod.rs
@@ -0,0 +1,15 @@
+//! 中断模块
+//! 
+//! 
+
+mod handler;
+mod context;
+
+/// 初始化中断相关的子模块
+/// 
+/// - [`handler::init`]
+/// - [`timer::init`]
+pub fn init() {
+    handler::init();
+    println!("mod interrupt initialized");
+}
--- os/src/main.rs
+++ os/src/main.rs
@@ -14,6 +14,7 @@
 mod console;
 mod panic;
 mod sbi;
+mod interrupt;
 
 // 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
@@ -23,5 +24,13 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
     println!("Hello rCore-Tutorial!");
-    panic!("end of rust_main")
+    
+    // 初始化各种模块
+    interrupt::init();
+
+    unsafe {
+        llvm_asm!("ebreak"::::"volatile");
+    };
+
+    panic!("end of rust_main");
 }
